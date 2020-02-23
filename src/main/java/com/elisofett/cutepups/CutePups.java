4267b8d78e6a96e906e432c94480a1eb6f7248bf
Quinn Brittain <qgbritt@gmail.com>
2020-02-22 19:05:24 -0500

    Add main mod class

---
+++ src/main/java/com/elisofett/cutepups/CutePups.java
@@ -0,0 +1,32 @@
+package com.elisofett.cutepups;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = CutePups.modId, name = CutePups.name, version = CutePups.version)
+public class CutePups {
+
+    public static final String modId = "tutorial";
+    public static final String name = "Tutorial Mod";
+    public static final String version = "1.0.0";
+
+    @Mod.Instance(modId)
+    public static CutePups instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        System.out.println(name + " is loading!");
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+}
