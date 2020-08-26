e215ad2bf915178dd5a3e91754bb68db9f336618
Quinn Brittain <qgbritt@gmail.com>
2020-08-26 17:26:00 -0400

    Update dog bowl

--- src/main/java/net/elisofett/cutepups/item/ModItems.java
+++ src/main/java/net/elisofett/cutepups/item/ModItems.java
@@ -6,15 +6,17 @@ import net.minecraftforge.registries.IForgeRegistry;
 
 public class ModItems {
     public static ItemBase dogFood = new ItemBase("dog_food").setCreativeTab(CreativeTabs.MATERIALS);
+    public static ItemBase dogBowl = new ItemBase("dog_bowl").setCreativeTab(CreativeTabs.MATERIALS);
     public static void register(IForgeRegistry<Item> registry) {
 
         registry.registerAll(
-                dogFood
+                dogFood, dogBowl
         );
     }
 
     public static void registerModels() {
         dogFood.registerItemModel();
+        dogBowl.registerItemModel();
     }
 
 
